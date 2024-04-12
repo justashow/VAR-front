af821571f01c5553374914de5e8b689718f2aff0
dnjs2721 <skadu66@gmail.com>
2024-04-12 15:07:41 +0900

    Refactor : package

--- krampoline/src/app/_component/Carousel/CarouselMain.tsx
+++ krampoline/src/app/_component/Carousel/CarouselMain.tsx
@@ -7,10 +7,10 @@ import Swiper from "swiper/bundle";
 import "swiper/css/bundle";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import Img1 from "../../../../public/CarouselImg/1.png";
-import Img2 from "../../../../public/CarouselImg/2.png";
-import Img3 from "../../../../public/CarouselImg/3.png";
-import Img4 from "../../../../public/CarouselImg/4.png";
+import Img1 from "/public/CarouselImg/1.png";
+import Img2 from "/public/CarouselImg/2.png";
+import Img3 from "/public/CarouselImg/3.png";
+import Img4 from "/public/CarouselImg/4.png";
 
 const CarouselMain = () => {
   useEffect(() => {
