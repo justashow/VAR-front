4370e5b48b1c30ab1874ff44baab7044dfd45fd2
dnjs2721 <skadu66@gmail.com>
2024-04-13 01:39:10 +0900

    Refactor : unoptimized_true

--- krampoline/src/app/(ALL)/viplist/_component/VipListBox.tsx
+++ krampoline/src/app/(ALL)/viplist/_component/VipListBox.tsx
@@ -24,6 +24,7 @@ const VipListBox = ({ vipBox }: Props) => {
               alt="ProfilePic"
               width={100}
               height={100}
+              unoptimized={true}
             />
           </Link>
         </div>
