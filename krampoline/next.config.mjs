6ac52e0db9b599ae06e06ed32515c4f9d75159fd
dnjs2721 <skadu66@gmail.com>
2024-04-20 03:16:28 +0900

    Refactor : config.js

--- krampoline/next.config.mjs
+++ krampoline/next.config.mjs
@@ -8,6 +8,14 @@ const nextConfig = {
   },
   reactStrictMode: false,
   output: "standalone",
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_KAKAO_API_KEY : process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    AWS_S3_BUCKET_REGION:process.env.AWS_S3_BUCKET_REGION,
+    AWS_S3_BUCKET_ACCESS_KEY_ID:process.env.AWS_S3_BUCKET_ACCESS_KEY_ID,
+    AWS_S3_BUCKET_SECRET_ACCESS_KEY:process.env.AWS_S3_BUCKET_SECRET_ACCESS_KEY,
+    NEXT_PUBLIC_WS_PROXY:process.env.NEXT_PUBLIC_WS_PROXY,
+  },
 };
 
 export default nextConfig;
