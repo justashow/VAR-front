2f483ba41c07ac2a73b4f82aac989bcb437d8161
dnjs2721 <skadu66@gmail.com>
2024-04-20 04:14:44 +0900

    Refactor : ㅣenv

--- krampoline/next.config.mjs
+++ krampoline/next.config.mjs
@@ -1,6 +1,7 @@
 import { truncateSync } from "fs";
 
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   images: {
     domains: ["k.kakaocdn.net", "t1.kakaocdn.net"],
@@ -8,14 +9,6 @@ const nextConfig = {
   },
   reactStrictMode: false,
   output: "standalone",
-  env: {
-    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-    NEXT_PUBLIC_KAKAO_API_KEY : process.env.NEXT_PUBLIC_KAKAO_API_KEY,
-    AWS_S3_BUCKET_REGION:process.env.AWS_S3_BUCKET_REGION,
-    AWS_S3_BUCKET_ACCESS_KEY_ID:process.env.AWS_S3_BUCKET_ACCESS_KEY_ID,
-    AWS_S3_BUCKET_SECRET_ACCESS_KEY:process.env.AWS_S3_BUCKET_SECRET_ACCESS_KEY,
-    NEXT_PUBLIC_WS_PROXY:process.env.NEXT_PUBLIC_WS_PROXY,
-  },
 };
 
 export default nextConfig;
